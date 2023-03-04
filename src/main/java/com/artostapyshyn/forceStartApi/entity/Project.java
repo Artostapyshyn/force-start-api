e4f406c2710b206cb4867ef3bff2b9d3fdfd2133
Artostapyshyn <artostapyshyn@gmail.com>
2023-03-04 23:49:37 +0200

    Entity mappings, spring security changes.

--- src/main/java/com/artostapyshyn/forceStartApi/entity/Project.java
+++ src/main/java/com/artostapyshyn/forceStartApi/entity/Project.java
@@ -1,10 +1,15 @@
 package com.artostapyshyn.forceStartApi.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,4 +42,9 @@ public class Project {
 	
 	@Column(name = "price", nullable = false)
 	private double price;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id")
+	@JsonBackReference
+    private User usr;
 }
--- src/main/java/com/artostapyshyn/forceStartApi/entity/User.java
+++ src/main/java/com/artostapyshyn/forceStartApi/entity/User.java
@@ -1,18 +1,23 @@
 package com.artostapyshyn.forceStartApi.entity;
 
 import java.util.Objects;
+import java.util.Set;
 
 import org.hibernate.Hibernate;
 
 import com.artostapyshyn.forceStartApi.enums.Role;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -45,6 +50,14 @@ public class User {
 	
 	@Column(name = "token")
 	private String token;
+	
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Resume resume;
+	
+	@OneToMany(mappedBy = "usr", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
+    private Set<Project> projects;
 	
 	@Override
 	public boolean equals(Object o) {
